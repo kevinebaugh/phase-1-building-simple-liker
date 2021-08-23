f1212161be2467ccc0609faf0bc473f4ece9380a
Kevin Ebaugh <kevinebaugh@gmail.com>
2021-08-22 20:22:42 -0400

    Done.

--- main.js
+++ main.js
@@ -4,14 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+function commenceLikeOrUnlike(e) {
+  mimicServerCall().then((res) => {
+    const alreadyLiked = e.target.classList.contains("activated-heart")
+    if (alreadyLiked == true) {
+      e.target.classList.remove("activated-heart")
+      e.target.innerText = EMPTY_HEART
+    } else {
+      e.target.classList.add("activated-heart")
+      e.target.innerText = FULL_HEART
+    }
 
+  }).catch((err) => {
+    console.error(err)
+    const errDiv = document.querySelector("#modal")
+    errDiv.classList.remove("hidden")
+    setTimeout(function() {
+      errDiv.classList.add("hidden")
+    }, 3000)
+  })
+}
+
+const likeRegions = document.querySelectorAll(".like")
+
+likeRegions.forEach(likeRegion => {
+  likeRegion.addEventListener("click", commenceLikeOrUnlike)
+})
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
